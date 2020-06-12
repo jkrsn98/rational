71c4f0c1b81dace0d94bd861b144978c76b4e0e8
BuildTools <justinkrasner98@gmail.com>
2020-06-12 00:10:29 -0400

    last of operator overloading and cleared test

--- Rational.cpp
+++ Rational.cpp
@@ -1,57 +1,70 @@
-#include "Rational.h"
+#include "rational.h"
 #include "gcd.h"
 #include "rational_exception.h"
 
 //------------------------------------------------------------------------------
 //constructor: (just ONE constructor -- uses default parameters)
-Rational::Rational(int num, int denom):num(num),denom(denom){
-  if(denom==0) {
+Rational::Rational(int num, int denom) : num(num), denom(denom)
+{
+  if (denom == 0)
+  {
     RationalException r("RationalException: can't instantiate Rational with denom 0");
-    std::cout<<r.getWhat()<<endl;
     throw r;
   }
-  int gcdx = gcd(this->num,this->denom);
-  this->num=num/gcdx;
-  this->denom=denom/gcdx;
+  int gcdx = gcd(this->num, this->denom);
+  this->num = num / gcdx;
+  this->denom = denom / gcdx;
 }
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 //getters:
-int Rational::getNumerator() const {return num;}
-int Rational::getDenominator() const {return denom;}
+int Rational::getNumerator() const { return num; }
+int Rational::getDenominator() const { return denom; }
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 //add & addInPlace:
-Rational Rational::operator +(const Rational &a){
-  return Rational(num * a.denom + a.num * denom, denom * a.denom);
+Rational operator+(const Rational &r1, const Rational &r2)
+{
+  return Rational(r1.getNumerator() * r2.getDenominator() +
+                      r2.getNumerator() * r1.getDenominator(),
+                  r1.getDenominator() * r2.getDenominator());
 }
-Rational& Rational::operator +=(const Rational &a){
-  int newNum = this->num*a.denom + a.num*this->denom;
+
+Rational &Rational::operator+=(const Rational &a)
+{
+  int newNum = this->num * a.denom + a.num * this->denom;
   int newDenom = this->denom * a.denom;
-  this->num = newNum/gcd(newNum,newDenom);
-  this->denom = newDenom/gcd(newNum,newDenom);
+  this->num = newNum / gcd(newNum, newDenom);
+  this->denom = newDenom / gcd(newNum, newDenom);
   return *this;
 }
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 //sub & subInPlace:
-Rational Rational::operator -(const Rational &a){
-  return Rational(num * a.denom - a.num * denom, denom * a.denom);
+Rational operator-(const Rational &r1, const Rational &r2)
+{
+  return Rational(r1.getNumerator() * r2.getDenominator() -
+                      r2.getNumerator() * r1.getDenominator(),
+                  r1.getDenominator() * r2.getDenominator());
 }
-Rational& Rational::operator -=(const Rational &a){
-  int newNum = this->num*a.denom - a.num*this->denom;
+Rational &Rational::operator-=(const Rational &a)
+{
+  int newNum = this->num * a.denom - a.num * this->denom;
   int newDenom = this->denom * a.denom;
-  this->num = newNum/gcd(newNum,newDenom);
-  this->denom = newDenom/gcd(newNum,newDenom);
+  this->num = newNum / gcd(newNum, newDenom);
+  this->denom = newDenom / gcd(newNum, newDenom);
   return *this;
 }
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 //mul & mulInPlace:
-Rational Rational::operator *(const Rational &r){
-  return Rational( this->num * (r.num), this->denom * (r.denom) );
+Rational operator*(const Rational &r1, const Rational &r2)
+{
+  return Rational(r1.getNumerator() * (r2.getNumerator()),
+                  r1.getDenominator() * (r2.getDenominator()));
 }
-Rational& Rational::operator *=(const Rational &r){
+Rational &Rational::operator*=(const Rational &r)
+{
   this->num *= r.num;
   this->denom *= r.denom;
   return *this;
@@ -59,10 +72,13 @@ Rational& Rational::operator *=(const Rational &r){
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 //div & divInPlace:
-Rational Rational::operator /(const Rational &r){
-  return Rational( this->num * (r.denom),this->denom * (r.num) );
+Rational operator/(const Rational &r1, const Rational &r2)
+{
+  return Rational(r1.getNumerator() * (r2.getDenominator()),
+                  r1.getDenominator() * (r2.getNumerator()));
 }
-Rational& Rational::operator /=(const Rational &r){
+Rational &Rational::operator/=(const Rational &r)
+{
   this->num *= r.denom;
   this->denom *= r.num;
   return *this;
@@ -70,39 +86,54 @@ Rational& Rational::operator /=(const Rational &r){
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 //else:
-Rational Rational::neg() const{
-  Rational r(-1*num,denom);
+Rational Rational::operator-() const
+{
+  Rational r(-1 * this->getNumerator(), this->getDenominator());
   return r;
 }
 
-Rational Rational::inv() const{
-  if(num==0) {
+Rational Rational::inv() const
+{
+  if (num == 0)
+  {
     RationalException r("RationalException: can't instantiate Rational with denom 0");
-    std::cout<<r.getWhat()<<endl;
     throw r;
   }
-  int newNum=denom;
-  int newDenom=num;
-  Rational r(newNum,newDenom);
+  int newNum = denom;
+  int newDenom = num;
+  Rational r(newNum, newDenom);
   return r;
 }
 
-bool Rational::equals(const Rational &r) const{
-  double x = (float) (this->num) / (float) (this->denom);
-  double y = (float) (r.num) / (float) (r.denom);
-  return (x==y)? true: false;
+bool operator==(const Rational &r1, const Rational &r2)
+{
+  double x = (float)(r1.getNumerator()) / (float)(r1.getDenominator());
+  double y = (float)(r2.getNumerator()) / (float)(r2.getDenominator());
+  return (x == y) ? true : false;
 }
 
-int Rational::compareTo(const Rational &r) const{
-  double x = (float) (this->num) / (float) (this->denom);
-  double y = (float) (r.num) / (float) (r.denom);
-  if(x>y) return 1;
-  if(x<y) return -1;
-  if(x==y) return 0;
+int Rational::compareTo(const Rational &r) const
+{
+  double x = (float)(this->num) / (float)(this->denom);
+  double y = (float)(r.num) / (float)(r.denom);
+  if (x > y)
+    return 1;
+  if (x < y)
+    return -1;
+  if (x == y)
+    return 0;
 }
 //------------------------------------------------------------------------------
+std::ostream &operator<<(std::ostream &os, const Rational &r)
+{
+  r.print(os);
+  return os;
+}
 
-void Rational::print(std::ostream &os) const{
-  if(this->denom!=1) os<<this->num<<"/"<<this->denom;
-  else os<<this->num;
+void Rational::print(std::ostream &os) const
+{
+  if (getDenominator() != 1)
+    os << getNumerator() << "/" << getDenominator();
+  else
+    os << getNumerator();
 }
--- rationaltest.cpp
+++ rationaltest.cpp
@@ -4,12 +4,6 @@
 using namespace std;
 
 int main(){
-  Rational r1(3,6);
-  Rational r2(1,2);
-
-  Rational r3 = r1+r2;
-  std::cout<<r3<<endl;
-
 
   return 0;
 }
